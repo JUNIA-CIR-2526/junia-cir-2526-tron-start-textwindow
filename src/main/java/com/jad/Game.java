0d71304d0883a7a50ef081c84743b156b45f6882
Luca Sfiotti <sfiottiluca@gmail.com>
2025-10-09 21:27:02 +0200

    LightCycle et une partie de Game qui gère la grille les déplacements ( problème avec les commandes de la bibliothèque pour l'instant )

--- src/main/java/com/jad/Game.java
+++ src/main/java/com/jad/Game.java
@@ -1,7 +1,55 @@
 package com.jad;
 
+import com.jad.textwindow.TextWindow;
+
+import java.awt.event.KeyEvent;
+
+
 public class Game {
+    private int width = 80;
+    private int height = 40;
+    private TextWindow tw;
+    private char[][] grid;
+    private LightCycle player1;
+    private LightCycle player2;
+    private int gameOver;
+
     public Game() {
-        Dimension dimension;
+        tw = new TextWindow();
+        grid = new char[width][height];
+
+        player1 = new LightCycle(20, height / 2, 1, '§', '#');
+        player2 = new LightCycle(60, height / 2, 3, '§', '#');
+
+        gameOver = 0;
+
+
     }
+
+    private void inputSettings() {
+        tw.addKeyboardListener(KeyEvent.VK_UP, "p1_up");
+        tw.addKeyboardListener(KeyEvent.VK_RIGHT, "p1_right");
+        tw.addKeyboardListener(KeyEvent.VK_DOWN, "p1_down");
+        tw.addKeyboardListener(KeyEvent.VK_LEFT, "p1_left");
+
+        tw.addKeyboardListener(KeyEvent.VK_Z, "p2_up");
+        tw.addKeyboardListener(KeyEvent.VK_D, "p2_right");
+        tw.addKeyboardListener(KeyEvent.VK_S, "p2_down");
+        tw.addKeyboardListener(KeyEvent.VK_Q, "p2_left");
+
+        if (tw.isOn("p1_up")) {
+            player1.setDirection(0);
+        }
+        if (tw.isOn("p1_right")) {
+            player1.setDirection(1);
+        }
+        if (tw.isOn("p1_down")) {
+            player1.setDirection(2);
+        }
+        if (tw.isOn("p1_left")) {
+            player1.setDirection(3);
+        }
+    }
+
+
 }
--- src/main/java/com/jad/LightCycle.java
+++ src/main/java/com/jad/LightCycle.java
@@ -1,5 +1,60 @@
 package com.jad;
 
-public class LightCycle {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LightCycle {
+    private int x;
+    private int y;
+    private int direction;
+    private char player;
+    private char walls;
+    private List<Position> trail;
+
+    public LightCycle(int x, int y, int direction, char player, char walls) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.player = player;
+        this.walls = walls;
+        this.trail = new ArrayList<>();
+    }
+
+    public void move() {
+        trail.add(new Position(x, y));
+
+        if (direction == 0) {
+            y = y - 1;
+        } else if (direction == 1) {
+            x = x + 1;
+        } else if (direction == 2) {
+            y = y + 1;
+        } else if (direction == 3) {
+            x = x - 1;
+        }
+    }
+
+    public void setDirection(int newDirection) {
+        this.direction = newDirection;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getPlayer() {
+        return player;
+    }
+
+    public char getWalls() {
+        return walls;
+    }
+
+    public List<Position> getTrail() {
+        return trail;
+    }
 }
